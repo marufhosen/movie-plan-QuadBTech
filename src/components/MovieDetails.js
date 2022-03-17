d07e66d8adfb911139439d33bebf3e7434f4f47a
Maruf Islam <maruf15-1591@diu.edu.bd>
2022-03-18 03:09:24 +0600

    update public folder

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import TicketForm from "./TicketForm";
 
 const MovieDetails = () => {
   const [singleMovie, setSingleMovie] = useState({});
